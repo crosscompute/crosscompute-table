b90fc37bf0fdd6d125a0fbe4a77e853d750a1857
Roy <rhh@crosscompute.com>
2018-01-13 13:21:50 -0500

    Support compressed CSVs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst'])
 setup(
     name='crosscompute-table',
-    version='0.7.3',
+    version='0.7.4',
     description='Table data type plugin for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
@@ -31,9 +31,9 @@ setup(
         'pytest-runner',
     ],
     install_requires=[
-        'invisibleroads-macros>=0.9.1',
-        'invisibleroads-uploads>=0.4.1',
-        'crosscompute>=0.7.3',
+        'invisibleroads-macros>=0.9.2',
+        'invisibleroads-uploads>=0.4.2.1',
+        'crosscompute>=0.7.4.1',
         'pandas',
     ],
     tests_require=[
