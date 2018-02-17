1929ec6d8fbc52ada22efbd286a2d6cd0250092a
Roy Hyunjin Han <rhh@crosscompute.com>
2018-02-17 19:37:43 +0000

    Update dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst'])
 setup(
     name='crosscompute-table',
-    version='0.7.5',
+    version='0.7.6',
     description='Table data type plugin for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
@@ -31,9 +31,9 @@ setup(
         'pytest-runner',
     ],
     install_requires=[
-        'invisibleroads-macros>=0.9.3',
-        'invisibleroads-uploads>=0.4.2.1',
-        'crosscompute>=0.7.5',
+        'invisibleroads-macros>=0.9.4.2',
+        'invisibleroads-uploads>=0.4.2.2',
+        'crosscompute>=0.7.6',
         'pandas',
     ],
     tests_require=[
